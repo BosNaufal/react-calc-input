fc502d2d3d65b771701592f74e23239a88ddc789
Naufal <bosnaufalemail@gmail.com>
2016-04-04 09:04:05 +0700

    Publish to NPM

--- src/react-calc-input.js
+++ src/react-calc-input.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import readable from './readable-number.js';
+import readable from 'readable-number';
 
 
 let CalcInput = React.createClass({
